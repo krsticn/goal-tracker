4b0c81557bae1a65c4d27bdb220836de941a858c
Krsticn <krsticn94@gmail.com>
2022-05-17 14:50:43 +0200

    In GoalIput.css a class is aded to display invalid input in input field. In GoalInput component, new state is aded that will act as a toggle for clas styling. On input field container, class is dynamicly added

--- src/components/GoalInput/GoalInput.js
+++ src/components/GoalInput/GoalInput.js
@@ -4,19 +4,27 @@ import "./GoalInput.css";
 
 const GoalInput = (props) => {
   const [entredGoals, setEntredGoals] = useState("");
+  const [isValid, setIsValid] = useState(true);
 
   const goalInputHandler = (event) => {
+    if (event.target.value.trim() > 0) {
+      setIsValid(true);
+    }
     setEntredGoals(event.target.value);
   };
 
   const submitHandler = (event) => {
     event.preventDefault();
+    if (entredGoals.trim().length === 0) {
+      setIsValid(false);
+      return;
+    }
     props.onAddGoal(entredGoals);
   };
 
   return (
     <form onSubmit={submitHandler}>
-      <div className="form-control">
+      <div className={`form-control ${!isValid ? "invalid" : ""}`}>
         <label>What's your goal?</label>
         <input type="text" onChange={goalInputHandler}></input>
       </div>
